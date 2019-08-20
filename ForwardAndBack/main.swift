7491557857eb29ed5646c04f4d4b0ad73bfe2259
Jens Nerup <jens@makecph.com>
2019-08-20 23:00:47 +0200

    Initial Commit

---
+++ ForwardAndBack/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ForwardAndBack
+//
+//  Created by Jens Nerup on 20/08/2019.
+//  Copyright © 2019 MAKE//CPH. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
