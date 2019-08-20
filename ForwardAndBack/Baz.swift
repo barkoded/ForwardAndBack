16a28c7ab06ac9ac32d5354f2ddbd5408c53b2f8
Jens Nerup <jens@makecph.com>
2019-08-20 23:18:07 +0200

    N/A

--- ForwardAndBack/Baz.swift
+++ ForwardAndBack/Baz.swift
@@ -3,10 +3,11 @@ import Foundation
 public final class Baz {
 
     func executeFoo(_ foo: MKCFoo) {
-        // This line below will not work
+        // This line below will not work as long as Bar.swift has the following @objc(MKCBar) attribute.
         // foo.bar
         // No try to replace @objc(MKCBar) in Bar.swift with just @objc and update the forward decleration in MKCfoo.h and correct the type in MKCfoo.m and now the line below will work but I've lost the prefix :-(
         // foo.bar
+
     }
 
 }
